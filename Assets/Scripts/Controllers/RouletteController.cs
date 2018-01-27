659fd85bd137943b67fd88cf5901e3d03f895d1f
Daniel Hernandez <LDHrez@users.noreply.github.com>
2018-01-27 14:32:43 -0600

    correct Roulette
    som chance of class RouletteController class

--- Assets/Scripts/Controllers/RouletteController.cs
+++ Assets/Scripts/Controllers/RouletteController.cs
@@ -8,25 +8,24 @@ public class RouletteController : Singleton<RouletteController> {
     private Notifier notifier;
     public const string ON_SELECTED_INFECTED = "OnSelectedInfected";
 
-    public Sprite imagePlayer;
+    //public Sprite imagePlayer;
     //public AudioClip audio;
 
-    private AudioSource audioSource;
+    //private AudioSource audioSource;
 
-    private int numPlayers;
-    private Image[] playersCard;
+    public int numPlayers;
+    //private Image[] playersCard;
     //private RuletaController ruleta;
 
     private int currentPlayer;
     private int playerSelected;
     private int lastCurrent;
     
-    
 
     // Use this for initialization
     void Start () {
         notifier = new Notifier();
-        audioSource = GetComponent<AudioSource>();
+        //audioSource = GetComponent<AudioSource>();
         lastCurrent = 0;
         currentPlayer = 0;
 
@@ -39,30 +38,37 @@ public class RouletteController : Singleton<RouletteController> {
         //}
     }
 
-    public void Initialize() 
+    public void Initialize(int numPlayers) 
     {
-        numPlayers = transform.childCount; 
-        TimerControl(240f, 2, 0f);
-        TimerControl(250f, 2, 480f);
-        TimerControl(260.86f, 2, 980f);
-        TimerControl(272.72f, 2, 1501.72f);
-        TimerControl(285.7f, 2, 2047.16f);
-        TimerControl(300f, 2, 2618.56f);
-        TimerControl(315.78f, 2, 3218.56f);
-        TimerControl(333.32f, 2, 3850.12f);
-        TimerControl(352.94f, 2, 4516.76f);
-        TimerControl(375f, 2, 5222.64f);
-        TimerControl(400f, 2, 5972.64f);
-        TimerControl(428.56f, 2, 6772.64f);
-        TimerControl(461.52f, 2, 7629.76f);
-        TimerControl(500f, 2, 8552.8f);
-        Invoke("Selected", 10f);
+
+        if (numPlayers != 0)
+        {
+            TimerControl(240f, 2, 0f);
+            TimerControl(250f, 2, 480f);
+            TimerControl(260.86f, 2, 980f);
+            TimerControl(272.72f, 2, 1501.72f);
+            TimerControl(285.7f, 2, 2047.16f);
+            TimerControl(300f, 2, 2618.56f);
+            TimerControl(315.78f, 2, 3218.56f);
+            TimerControl(333.32f, 2, 3850.12f);
+            TimerControl(352.94f, 2, 4516.76f);
+            TimerControl(375f, 2, 5222.64f);
+            TimerControl(400f, 2, 5972.64f);
+            TimerControl(428.56f, 2, 6772.64f);
+            TimerControl(461.52f, 2, 7629.76f);
+            TimerControl(500f, 2, 8552.8f);
+            Invoke("Selected", 10f);
+        }
+        else
+        {
+            Debug.Log("Players don´t exist");
+        }
     }
 	
 
     void ChangeSelected()
     {
-        Debug.Log(numPlayers);
+        //Debug.Log(numPlayers);
         currentPlayer = Random.Range(0, numPlayers);
         if(currentPlayer == lastCurrent)
         {
@@ -75,11 +81,11 @@ public class RouletteController : Singleton<RouletteController> {
             lastCurrent = currentPlayer;
         }
 
-        if(!audioSource.isPlaying)
+        /*if(!audioSource.isPlaying)
         {
             
             audioSource.Play();
-        }
+        }*/
     }
 
     void Selected()
--- Assets/Scripts/Logic/GameManager.cs
+++ Assets/Scripts/Logic/GameManager.cs
@@ -110,4 +110,5 @@ public class GameManager : Singleton<GameManager>
     {
         notifier.UnsubcribeAll();
     }
+
 }
