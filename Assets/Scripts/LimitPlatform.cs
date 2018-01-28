144e4993c17b4aaf62a1595f302cdff81802c98e
Daniel Hernandez <LDHrez@users.noreply.github.com>
2018-01-27 23:34:12 -0600

    asset in the project

--- Assets/Scripts/LimitPlatform.cs
+++ Assets/Scripts/LimitPlatform.cs
@@ -4,25 +4,35 @@ using UnityEngine;
 
 public class LimitPlatform : MonoBehaviour {
 
-    
 
-	// Use this for initialization
-	void Start () {
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    private int force;
+    [SerializeField]
+    private float dangerAmount;
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("other " + other.name);    
+        if (other.tag == "Player")
+        {
+            Debug.Log("other " + other.name);
+            Rigidbody rigidbody = other.GetComponent<Rigidbody>();
+            rigidbody.sleepThreshold = 10f;
+            rigidbody.AddForceAtPosition(new Vector3(-1f, 0f, -1f) * force, Vector3.zero);
+
+        }
     }
     private void OnTriggerStay(Collider other)
     {
-        
+        if (other.tag == "Player")
+        {
+            Rigidbody rigidbody = other.GetComponent<Rigidbody>();
+            rigidbody.sleepVelocity = 1;
+            Player player = other.GetComponent<Player>();
+            player.UpdateHealth(-dangerAmount);
+            
+
+        }
+
     }
     private void OnTriggerExit(Collider other)
     {
