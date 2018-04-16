2242949e1c8e90e367a1683e177afb6cdd2ea100
Jaime Hernández <jaimevazz@gmail.com>
2018-04-16 14:38:18 -0500

    Showroom shit and some fixes

--- Assets/_ChickenFlu/Scripts/Models/FluTransmission.cs
+++ Assets/_ChickenFlu/Scripts/Models/FluTransmission.cs
@@ -6,6 +6,7 @@ public class FluTransmission : MonoBehaviour
 {
     [SerializeField] private float infectSafeTime = 1.0f;
     [SerializeField] private GameObject particleFX;
+    [SerializeField] private float headHeight = 0.6f;
     [SerializeField] private AudioClip audioFX;
 
     private Player player;
@@ -24,15 +25,29 @@ public class FluTransmission : MonoBehaviour
             Player other = collision.transform.GetComponent<Player>();
             if (other.CanBeInfected)
             {
+
                 //Debug.Log("Flu Trans - Transmited to: " + other.transform.name);    
 				GameManager.Instance.Infect(other.Id);
-                AudioManager.Instance.PlayOneShoot(audioFX, other.transform.position);
-                Instantiate(particleFX, other.transform.position + new Vector3(0, 1), Quaternion.identity);
+                this.PlayEffect(other);
                 StartCoroutine(this.InfectSafe());
             }
         }
 	}
+    private void PlayEffect(Player other) 
+    {
+        AudioManager.Instance.PlayOneShoot(audioFX, other.transform.position);  
+        Instantiate(particleFX, other.transform.position + new Vector3(0, 1), Quaternion.identity);
+    }
+    public void PlayEffect() 
+    {
+        this.player.Mutate(PlayerState.Infected);
+        Vector3 headPos = this.transform.position + new Vector3(0, headHeight);
+        Vector3 offset = headPos - Camera.main.transform.position;
+        offset = -offset.normalized;
+        Instantiate(particleFX, headPos + offset, Quaternion.identity);
+        AudioManager.Instance.PlayOneShoot(audioFX, this.transform.position);  
 
+    }
     private IEnumerator InfectSafe()
     {
         this.player.CanBeInfected = false;
