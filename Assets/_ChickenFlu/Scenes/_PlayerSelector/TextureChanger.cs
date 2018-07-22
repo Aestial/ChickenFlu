c8759ab3b67168f5b94d659da0f832314bfa6540
Jaime Hernandez <jaimevazz@gmail.com>
2018-07-22 16:32:42 -0500

    New Textures
    Doctors

---
+++ Assets/_ChickenFlu/Scenes/_PlayerSelector/TextureChanger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureChanger : MonoBehaviour 
+{
+    [SerializeField] private Renderer renderer;
+    [SerializeField] private Texture2D texture;
+
+	// Use this for initialization
+	void Start () 
+    {
+        this.renderer.material.mainTexture = this.texture;
+	}
+}
